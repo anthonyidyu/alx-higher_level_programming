83212f22bdb30ce450b82957bd3c8cdecec0c710
anthonyidyu <anthonysynod@gmail.com>
2022-10-03 14:43:55 +0000

    0x01-python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0:
     remainder = number % -10
 else:
     remainder = number % 10
-print('Last digit of' , number, 'is', remainder, end=' ')
+print('Last digit of' , number, 'is', remainder, end=',')
 if remainder > 5:
     print('and is greater than 5')
 elif remainder == 0:
