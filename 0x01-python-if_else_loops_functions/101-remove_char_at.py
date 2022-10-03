ea9b1e4cccc6272de81b2e9a6ca6593092d4a616
Anthony Idyu <anthonysynod@gmail.com>
2022-10-03 01:56:13 +0100

    0x01-python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n >= 0:
-       newstr = str[:n] + str[n + 1:]
-       return (newstr)
-    else:
-        return (str)
+        str = str[:n] + str[(n + 1):]
+    return str
