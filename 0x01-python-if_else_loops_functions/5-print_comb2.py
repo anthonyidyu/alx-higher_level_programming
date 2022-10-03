a71c2a1fedfc438a66fadf51ba732af90c7f431f
anthonyidyu <anthonysynod@gmail.com>
2022-10-03 14:52:45 +0000

    0x01-python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=',')
-print("{:02d}".format(i + 1))
+for i in range(100):
+    if i == 99:
+        print(i)
+    elif i < 99:
+        print('{:02d}, '.format(i), end='')
